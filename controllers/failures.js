957b096d15d072e02accd7e987acd78610ef706b
Jose Alejandro Peña <alejandro@apptory.com.co>
2017-01-12 23:01:21 -0500

    API Get Failures
    API Get Failures

--- controllers/failures.js
+++ controllers/failures.js
@@ -1,36 +1,10 @@
-var mongoose = require('mongoose');
-var model = require('../models/failure');
-var Failure = mongoose.model('Failure');
+var FailureModel = require('../models/failure');
 
 //GET - Return all registers
 exports.findAll = function(req, res){
-    Failure.find(function(err, failures){
-        if(err) res.send(500, err.message);
-        console.log('GET /failures')
-        res.status(200).jsonp(failures);
-    });
+    FailureModel.getFailures(function(error, data)
+		{
+            console.log('GET /failures')
+            res.status(200).jsonp({ success: true, status_code: 200, status_message: "Failures", failures: data});
+		});
 };
-
-//GET - Return a register with specified ID
-//export.findById = function(req, res){
-//    Profile.findById(req.params.id, function(err, failure){
-//        if(err) return res.send(500, err.message);
-//        console.log('GET /profiles' + req.params.id);
-//        res.status(200).jsonp(profile);
-//    })
-//};
-
-//POST - Insert a new register
-//exports.add = function(req, res) {
-// console.log('POST');
-// console.log(req.body);
-// var client = new Client({
-// name: req.body.name,
-// email: req.body.email,
-// genre: req.body.genre
-// });
-// client.save(function(err, client) {
-// if(err) return res.send(500, err.message);
-// res.status(200).jsonp(client);
-// });
-//};
